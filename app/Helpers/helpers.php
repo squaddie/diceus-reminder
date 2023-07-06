b4ddbb9661d30820219a8ad80199338faf8939f4
ka4piton <squaddie.pride@gmail.com>
2023-07-06 21:23:13 +0300

    added simple path finder

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,9 +1,29 @@
 <?php
 
+const APP_FOLDER = 'app';
+
+function appPath(string $filePath = '')
+{
+    $pathSegments = explode(DIRECTORY_SEPARATOR , __DIR__);
+    $path = [];
+
+    foreach ($pathSegments as $key => $pathSegment) {
+        $path[] = $pathSegment;
+
+        if (isset($pathSegments[$key + 1])) {
+            if ($pathSegments[$key + 1] == APP_FOLDER) {
+                return implode('/', $path) . "/{$filePath}";
+            }
+        }
+    }
+
+    return '';
+}
+
 function env($key)
 {
     $mappedEnvFileData = [];
-    $envFile = file_get_contents('.env');
+    $envFile = file_get_contents(appPath('.env'));
     $envFile = array_filter(explode(PHP_EOL, $envFile));
     array_map(function ($row) use (&$mappedEnvFileData) {
         $segments = explode('=', $row);
